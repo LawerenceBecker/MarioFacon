7c78613f7d9f52ae84a99473d61c7dc703f58b66
Lawerence Becker <95592713+LawerenceBecker@users.noreply.github.com>
2022-01-31 14:55:41 +0000

    Added Pygame basic set up.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Mario Stile
 Means Mario Style in Itallian
+
+3.23.12
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import pygame as pg
+import sys
 
+
+class Game():
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((1280,720))
+        pg.display.set_caption('MarioStile')
+        self.clock = pg.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.QUIT
+                    sys.exit()
+
+            self.screen.fill('black')
+            pg.display.update()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
